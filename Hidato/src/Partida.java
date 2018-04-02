fcd3125360a4a002a3bf6751df34bf52589aa1bd
jiaxiangcheng <jiaxiangcheng7@gmail.com>
2018-04-02 16:35:58 +0100

    created clases

---
+++ Hidato/src/Partida.java
@@ -0,0 +1,31 @@
+
+public class Partida {
+	private dificultat dif;
+	private int puntacio;
+	private int Date;
+	private String[][] estat;
+	
+	public void checkCorrecte() {
+		
+	}
+	
+	public void acabarPartida() {
+			
+	}
+	
+	public void reset() {
+		
+	}
+	public void ferJugada() {
+			
+	}
+	public void demanarPista() {
+		
+	}
+	public void demanarSolucio() {
+		
+	}
+	public void reprendrePartida() {
+		
+	}
+}
