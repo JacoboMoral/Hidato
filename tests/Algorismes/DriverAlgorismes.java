e83d5e5551810d68711f8a198b49344a25ad0663
Jacobo Moral <jacobo.moral@gmail.com>
2018-04-24 22:16:05 +0200

    algorismes drivers, not implemented yet but prepared

--- tests/Algorismes/DriverAlgorismes.java
+++ tests/Algorismes/DriverAlgorismes.java
@@ -1,20 +1,53 @@
+package Algorismes;
 import java.util.Scanner;
+
+import main.domain.com.hidato.Algorismes;
 
 
 public class DriverAlgorismes {
+	private static int numeroTests = 14;
+	
 	public void testConstructor() {
-		
 	}
 	
 	
     public static void main(String[] args) throws Exception{
 		System.out.println("Aquest es el driver de algorismes, indica quina funcio vols probar");
-		//llistaTests();
+		llistaTests();
+		int req = -1;
+		req = getRequest();
+		while (req == -1 || req > numeroTests) {
+			System.out.println("Si us plau, entra un dels nombres corresponents a la funcio que vols provar\n");
+			llistaTests();
+			req = getRequest();
+		}
+		tractaRequest(req);
 		
 		
 	}
 	
-	public void llistaTests() {
+	private static void tractaRequest(int req) {
+		switch (req) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+			case 11:
+			case 12:
+			case 13:
+			case 14:
+			default:
+		}
+	}
+
+
+	public static void llistaTests() {
 		System.out.println("1: Constructor");
 		System.out.println("2: Modificar hidato");
 		System.out.println("3: Solucionar");
@@ -31,14 +64,29 @@ public class DriverAlgorismes {
 		System.out.println("14: generarMatriuCompleta");
 	}
 	
-	private String readLine() {		
+	private static String readLine() {		
 		Scanner input = new Scanner(System.in);
 		String req = input.nextLine();
 		return req;
 	}
 	
-	private int getRequest() {
+	private static int getRequest() {
 		String req = readLine();
-		if (StringUtils.isNumeric(req)) return 1;
+		if (isNumber(req)) return Integer.parseInt(req);
+		
+		else return -1;
 	}
+
+	private static boolean isNumber(String s) {
+		boolean validNumber = false;
+		try {
+			Integer.parseInt(s);
+			//si arriba fins aqui, es un numero
+			validNumber = true;
+		} catch (NumberFormatException e) {
+			//si entra aqui, alhesores ha saltat l'excepcio de que no es pot convertir en numero
+		}
+		return validNumber;
+	}
+	
 }
---
+++ tests/Algorismes/HidatoStub.java
@@ -0,0 +1,5 @@
+package Algorismes;
+
+public class HidatoStub {
+
+}
